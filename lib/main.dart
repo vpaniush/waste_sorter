8568da7bfa727fb93fa5e42658320a48e10e90b9
vpaniush <volodymyr2507@gmail.com>
2021-04-15 21:10:15 +0300

    adds tfilte response parser; setups firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'presentation/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     EasyLocalization(
       child: App(),
